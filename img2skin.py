2bb9c2d28c4055e55d52ccb73ce5bc10adc16f0a
Paweł Szmergała <pawelszme@gmail.com>
2022-08-23 19:41:50 +0200

    Implement applying default skin template

--- img2skin.py
+++ img2skin.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 import argparse
-import sys
 import io
+import sys
+from copy import deepcopy
 from PIL import Image
 
+import utils
 
 # coords of parts of mc skin
 # front
@@ -38,7 +40,7 @@ img_coords = [IMG_HEAD, IMG_RIGHT_ARM, IMG_BODY,
               IMG_LEFT_ARM, IMG_RIGHT_LEG, IMG_LEFT_LEG]
 
 
-def img2skin(skin: Image, img_front: Image, img_back: Image) -> Image:
+def img2skin(img_front: Image, img_back: Image, template: Image = None) -> Image:
     """Paste `img_front` and `img_back` onto `skin`.
 
     Args:
@@ -49,6 +51,10 @@ def img2skin(skin: Image, img_front: Image, img_back: Image) -> Image:
     Returns:
         Image: _description_
     """
+    skin = deepcopy(template)
+    if skin is None:
+        skin = Image.fromarray(utils.template)
+
     img_front = img_front.resize((24, 32))
     for f_coord, i_coord in zip(front_coords, img_coords):
         img_region = img_front.crop(i_coord)
@@ -65,20 +71,21 @@ def img2skin(skin: Image, img_front: Image, img_back: Image) -> Image:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "template", help="Skin texture into which given images will be pasted.")
-    parser.add_argument(
         "front", help="Image that will be on front of the skin.")
     parser.add_argument(
         "back", help="Image that will be on back of the skin.")
     parser.add_argument(
+        "-t", "--template", help="Optional skin template. If absent, a default one will be used.",
+        required=False)
+    parser.add_argument(
         "-o", "--out", help="File name the skin will be saved to.", required=False)
     args = parser.parse_args()
 
-    res: Image = img2skin(Image.open(args.template), Image.open(
-        args.front), Image.open(args.back))
+    res: Image = img2skin(Image.open(args.front), Image.open(args.back),
+                          None if args.template is None else Image.open(args.template))
     if args.out is None:
         bin_res = io.BytesIO()
-        res.save(bin_res, format='png')
+        res.save(bin_res, format="png")
         sys.stdout.buffer.write(bin_res.getvalue())
     else:
         res.save(args.out)
